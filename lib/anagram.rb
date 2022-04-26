4fc8bddb92c82d86b6db635cf30dd9c981be08a7
Mark <mark.n.bacon.jr@gmail.com>
2022-04-25 23:34:15 -0400

    commit tree bc786a7086623686944da30a4931af96ff6e6c91

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+require "pry"
+class Anagram
+
+    def initialize word
+        @word = word
+    end
+
+    def match arr
+        newArr = arr.filter do |item|
+            item.chars.sort == @word.chars.sort
+        end
+        # binding.pry
+        # newArr
+    end
+
+
+
+
+end
